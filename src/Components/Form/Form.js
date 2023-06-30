4920fcd9304ba8edd22fd4d68ef25b880ec132e2
Gabriel Soares <gabrsoares@outlook.com>
2023-06-30 16:54:11 -0300

    finish css & form component

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -6,10 +6,15 @@ class Form extends React.Component {
         super(props);
 
         this.state = {
-            username: '',
-            age:'',
+            nome: '',
+            idade:'',
             cpf: '',
-            civil: ''
+            civil: '',
+            genero: '',
+            isSubmitted: false,
+            validateEmpty: {
+                isEmpty: true
+            }
         }
     }
 
@@ -19,7 +24,13 @@ class Form extends React.Component {
 
     handleSubmit = (event) => {
         event.preventDefault()
-        console.log("enviado!")
+        const newState = {isSubmitted: true}
+        if(this.state.nome.length !== 0 && this.state.idade.length !== 0 && this.state.cpf.length !==  0 && this.state.civil.length !== 0 && this.state.genero.length !== 0) {
+            newState.validateEmpty = {isEmpty: false}
+        }
+        this.setState(newState)
+        console.log("isEmpty:",this.state.validateEmpty.isEmpty)
+        console.log("isSubmitted:",this.state.isSubmitted)
     }
 
     render(){
@@ -28,16 +39,17 @@ class Form extends React.Component {
                 <div className="form">
                     <div className="loginInfo">
                         <div className="container name">
-                            <label htmlFor="username">Nome</label>
-                            <input onChange={(event)=> this.changeField(event.target.id, event.target.value)} type="text" placeholder="Digite seu nome" id="username"/>
+                            <label htmlFor="nome">Nome</label>
+                            <input onChange={(event)=> this.changeField(event.target.id, event.target.value)} type="text" placeholder="Digite seu nome" id="nome"/>
                         </div>
-                        <div className="container age">
-                            <label htmlFor="age">Idade</label>
-                            <input onChange={(event)=> this.changeField(event.target.id, event.target.value)} type="number" placeholder='Ex: 18' id="age"/>
+                        <div className="container idade">
+                            <label htmlFor="idade">Idade</label>
+                            <input onChange={(event)=> this.changeField(event.target.id, event.target.value)} type="number" placeholder='Ex: 18' id="idade"/>
                         </div>
                         <div className="container civil">
                             <label htmlFor="civil">Estado Civil</label>
-                            <select name="civil" id="civil">
+                            <select name="civil" id="civil" onChange={(event) => this.changeField(event.target.id, event.target.value)}>
+                                <option value=""></option>
                                 <option value="Solteiro(a)">Solteiro(a)</option>
                                 <option value="Casado(a)">Casado(a)</option>
                                 <option value="Viúvo(a)">Viúvo(a)</option>
@@ -46,32 +58,53 @@ class Form extends React.Component {
                         </div>
                         <div className="container cpf">
                             <label htmlFor="cpf">CPF</label>
-                            <input onChange={(event) => this.changeField(event.target.id, event.target.value)} type="text" placeholder='Digite seu CPF' id='cpf'/>
+                            <input maxLength={14} onChange={(event) => this.changeField(event.target.id, event.target.value)} type="text" placeholder='Digite seu CPF' id='cpf'/>
                         </div>
                     </div>
                     <div className='radio'>
                         <label>Gênero</label>
                         <div>
-                            <input type="radio" name="gender" value="Masculino" option="Masculino"/>
+                            <input type="radio" name="genero" value="Masculino" option="Masculino" onChange={(event) => this.changeField(event.target.name, event.target.value)}/>
                             <label>Masculino</label>
                         </div>
                         <div>
-                            <input type="radio" name="gender" value="Feminino" option="Feminino"/>
+                            <input type="radio" name="genero" value="Feminino" option="Feminino" onChange={(event) => this.changeField(event.target.name, event.target.value)}/>
                             <label>Feminino</label>
                         </div>
                         <div>
-                            <input type="radio" name="gender" value="Outros" option="Outros"/>
+                            <input type="radio" name="genero" value="Outros" option="Outros" onChange={(event) => this.changeField(event.target.name, event.target.value)}/>
                             <label>Outros</label>
                         </div>
                     </div>
                     <div className="validation">
-                        
+                        {this.state.validateEmpty.isEmpty && this.state.isSubmitted && <p>Preencha todos os campos!</p>}
                     </div>
                     <input type="submit" value="Cadastrar" id='cadButton'/>
                 </div>
-                <div className="results">
-                    
-                </div>
+                <div className="results" style={{display:(!this.state.validateEmpty.isEmpty && this.state.isSubmitted)?"flex":"none"}}>
+                    {(!this.state.validateEmpty.isEmpty && this.state.isSubmitted) && <>
+                        <div className="container">
+                            <p>Nome:</p>
+                            <p>{this.state.nome}</p>
+                        </div>
+                        <div className="container">
+                            <p>Idade:</p>
+                            <p>{this.state.idade}</p>
+                        </div>
+                        <div className="container">
+                            <p>Estado civil:</p>
+                            <p>{this.state.civil}</p>
+                        </div>
+                        <div className="container">
+                            <p>CPF:</p>
+                            <p>{this.state.cpf}</p>
+                        </div>
+                        <div className="container">
+                            <p>Gênero:</p>
+                            <p>{this.state.genero}</p>
+                        </div>
+                    </>}
+                </div>   
             </form>
         )
     }
